319914a74c2296d7b1a575a62b2bf588cef179cb
Apollo Rowe <1apollo.rowe@gmail.com>
2023-10-06 00:13:10 -0400

    file updates

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
+#! /bin/bash
+# #SBATCH -n 1
+# #SBATCH --gres=gpu:0
+# #SBATCH --mem 1G
+
+# module load python/3.9.12/
+# module load cuda12.1/blas/
+# module load cuda12.1/fft/
+# module load cuda12.1/toolkit/
+
+python run.py 100000
 #!/usr/bin/env bash
 
 python run.py 100000
