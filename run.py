6fdc41aead653ab330997c7e6672ea6fe16bce60
Apollo Rowe <1apollo.rowe@gmail.com>
2023-11-06 02:02:16 -0500

    removed lfs and cleaned up repo

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
-import deeplabcut
-import os
-import time
 import sys
+import os
 
 print(f'Using absolute path: {os.getcwd()}')
-print(f'Max iterations: {sys.argv[1]}')
-display_interval = int(sys.argv[1]) / 100
-print(f'Display interval: {sys.argv[2]}')
+abs_path = os.getcwd()
+dlc_path = abs_path + 'DeepLabCut/deeplabcut'
+if not os.path.exists(dlc_path):
+    dlc_path = abs_path + 'dlc/deeplabcut'
+sys.path.insert(0, '')
 
-project_path = os.getcwd() + '/MQP-Apollo-Rowe-2023-09-15/'
+start = sys.argv[1]
+
+import deeplabcut
+import time
+
+project = sys.argv[3]
+print(f'Project: {project}')
+
+project_path = os.getcwd() + project
 if not os.path.exists(project_path):
     print('Default project does not exist')
     exit()
@@ -18,33 +26,75 @@ if not os.path.exists(config_path):
     print('Configuration file not detected')
     exit()
 
-if not os.path.exists(project_path + 'training-datasets'):
-    print('Training dataset not found...')
-    print('Creating training dataset')
+if start == "train":
+    print(f'Max iterations: {sys.argv[2]}')
+    display_interval = int(sys.argv[2]) / 100
+    print(f'Display interval: {display_interval}')
+
+    if not os.path.exists(project_path + 'training-datasets'):
+        print('Training dataset not found...')
+        print('Creating training dataset')
+
+        start = time.time()
+        res = deeplabcut.create_training_dataset(
+            config=config_path,
+            net_type='resnet_50'
+        )
+
+        if res is None:
+            print('Failed to create training dataset')
+            exit()
+
+        end = time.time()
+        print(f'Created in {end - start}ms')
+
+    print('Training network')
 
     start = time.time()
-    res = deeplabcut.create_training_dataset(
+    deeplabcut.train_network(
         config=config_path,
-        net_type='resnet_50'
+        maxiters=int(sys.argv[1]),
+        saveiters=int(sys.argv[1]) / 10,
+        displayiters=display_interval,
+    )
+    end = time.time()
+
+    print(f'Training completed in {end - start}ms')
+
+elif start == "eval":
+    print('Attempting Evaluate...')
+
+    start = time.time()
+
+    deeplabcut.evaluate_network(
+        config=config_path,
+        Shuffles=[1],
+        plotting=True,
     )
 
-    if res is None:
-        print('Failed to create training dataset')
-        exit()
-
     end = time.time()
-    print(f'Created in {end - start}ms')
 
-print('Training network')
+    print(f'Finished eval in {end - start}ms')
 
-start = time.time()
-deeplabcut.train_network(
-    config=config_path,
-    maxiters=int(sys.argv[1]),
-    saveiters=int(sys.argv[1]) / 10,
-    displayiters=display_interval,
-)
-end = time.time()
+elif start == 'analyze':
+    print('Attempting Analyze...')
 
-print(f'Training completed in {end - start}ms')
+    videos = []
 
+    if not sys.argv[2] == '*':
+        for x in sys.argv:
+            index = x.find('.')
+            if index == -1:
+                continue
+            ending = x[index+1:]
+            if ending == 'mp4':
+                videos.append(x)
+    else:
+        videos.append(project_path + '/videos')
+
+    deeplabcut.analyze_videos(
+        config=config_path,
+        videos=videos,
+        save_as_csv=True,
+        videotype='mp4'
+    )
--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
-# #SBATCH -n 1
-# #SBATCH --gres=gpu:0
-# #SBATCH --mem 1G
 
-# module load python/3.9.12/
-# module load cuda12.1/blas/
-# module load cuda12.1/fft/
-# module load cuda12.1/toolkit/
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  #SBATCH -n 1
+  #SBATCH --gres=gpu:0
+  #SBATCH --mem 1G
 
-python run.py 100000
-#!/usr/bin/env bash
+  # module load python/3.9.12/
+  # module load cuda12.1/blas/
+  # module load cuda12.1/fft/
+  # module load cuda12.1/toolkit/
 
-python run.py 100000
+  python run.py eval 100000 /MQP-Wrist-Mouse-Apollo-Rowe-2023-10-05
+elif [[ "$OSTYPE" == "cygwin"* ]]; then
+  echo hi
+fi
